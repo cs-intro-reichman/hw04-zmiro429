7d3043fb4dab139d2e0c3d7ecbebca8bac7b0e67
zmiro429 <adam.zmiro@post.runi.ac.il>
2024-01-16 00:32:53 +0200

    first time a charm

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,26 +1,75 @@
+import java.util.Arrays;
+
 public class ArrayOps {
     public static void main(String[] args) {
-        
+
     }
-    
-    public static int findMissingInt (int [] array) {
+
+    public static int findMissingInt(int[] array) {
         // Write your code here:
+        int arrl = array.length;
+        for (int i = 0; i <= arrl; i++) {
+            int m = i;
+            boolean doesexist = false;
+            for (int j = 0; j < arrl; j++) {
+                if (m == array[j]) {
+                    doesexist = true;
+                    break;
+                }
+            }
+            if (doesexist == false) {
+                return m;
+            }
+        }
         return -1;
     }
 
-    public static int secondMaxValue(int [] array) {
+    public static int secondMaxValue(int[] array) {
         // Write your code here:
-        return 0;
+        int arrl = array.length;
+        int max = array[0], secondmax = array[0];
+        // this for loop to find the Max in the array
+        for (int i = 0; i < arrl; i++) {
+            for (int j = 0; j < arrl; j++) {
+                if (max < array[j])
+                    max = array[j];
+            }
+        }
+        for (int i = 0; i < arrl; i++) {
+            for (int j = 0; j < arrl; j++) {
+                int num = array[j];
+                if (num > secondmax && num < max)
+                    secondmax = num;
+            }
+        }
+        return secondmax;
     }
 
-    public static boolean containsTheSameElements(int [] array1,int [] array2) {
+    public static boolean containsTheSameElements(int[] array1, int[] array2) {
         // Write your code here:
-        return false;
+        String arr1 = Arrays.toString(array1);
+        String arr2 = Arrays.toString(array2);
+        for (int i = 0; i < arr1.length(); i++) {
+            if (arr2.indexOf(arr1.charAt(i)) == -1) {
+                return false;
+            }
+        }
+        for (int i = 0; i < arr2.length(); i++) {
+            if (arr1.indexOf(arr2.charAt(i)) == -1) {
+                return false;
+            }
+        }
+        return true;
     }
 
-    public static boolean isSorted(int [] array) {
+    public static boolean isSorted(int[] array) {
         // Write your code here:
-        return false;
+        String arr1 = Arrays.toString(array);
+        for (int i = 1; i < arr1.length(); i++) {
+            if (arr1.charAt(i - 1) > arr1.charAt(i))
+                return false;
+        }
+        return true;
     }
 
 }
--- StringOps.java
+++ StringOps.java
@@ -1,42 +1,81 @@
 public class StringOps {
     ////////////////////////////////////////////////////////////
-    //////                                               ///////
-    //////              Reminder:                        ///////
-    //////        allowed methods                        ///////
-    //////                                               ///////
-    //////        1.charAt(int index)                    ///////
-    //////        2.length()                             ///////
-    //////        3.substring(int start)                 ///////
-    //////        4.substring(int start,int ends)        ///////
-    //////        5.indexOf(String str)                  ///////
-    //////                                               ///////
-    //////        The rest are not allowed !             ///////
-    //////        if you want to use a different         ///////
-    //////        method, and you can implement          ///////
-    //////        it using material from the course      ///////
-    //////        you need to implement a version of     ///////
-    //////        the function by yourself.              ///////
-    //////                                               ///////
-    //////        see example for substring              ///////
-    //////        in Recitation 3 question 5             ///////
-    //////                                               ///////
+    ////// ///////
+    ////// Reminder: ///////
+    ////// allowed methods ///////
+    ////// ///////
+    ////// 1.charAt(int index) ///////
+    ////// 2.length() ///////
+    ////// 3.substring(int start) ///////
+    ////// 4.substring(int start,int ends) ///////
+    ////// 5.indexOf(String str) ///////
+    ////// ///////
+    ////// The rest are not allowed ! ///////
+    ////// if you want to use a different ///////
+    ////// method, and you can implement ///////
+    ////// it using material from the course ///////
+    ////// you need to implement a version of ///////
+    ////// the function by yourself. ///////
+    ////// ///////
+    ////// see example for substring ///////
+    ////// in Recitation 3 question 5 ///////
+    ////// ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+
     }
 
-    public static String capVowelsLowRest (String string) {
+    public static String capVowelsLowRest(String string) {
         // Write your code here:
-        return "";
+        char[] result = new char[string.length()];
+        for (int i = 0; i < string.length(); i++) {
+            char ch = string.charAt(i);
+            if (ch >= 'A' && ch <= 'Z') {
+                ch += 32; // Convert to lowercase
+            }
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                ch -= 32; // Convert to uppercase
+            }
+            result[i] = ch;
+        }
+        return new String(result);
     }
 
-    public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+    public static String camelCase(String string) {
+        String result = "";
+        boolean newWord = true;
+        for (int i = 0; i < string.length(); i++) {
+            char ch = string.charAt(i);
+            if (ch == ' ') {
+                newWord = true;
+                continue;
+            }
+            if (newWord && ch >= 'a' && ch <= 'z') {
+                ch -= 32; // Convert first letter of each word to uppercase
+            } else if (!newWord && ch >= 'A' && ch <= 'Z') {
+                ch += 32; // Convert remaining letters to lowercase
+            }
+            result += ch;
+            newWord = false;
+        }
+        return result;
     }
 
-    public static int[] allIndexOf (String string, char chr) {
+    public static int[] allIndexOf(String string, char chr) {
         // Write your code here:
-        return new int[1];
+        int sum = 0, j = 0;
+        // this for loop to know how big is the array
+        for (int i = 0; i < string.length(); i++) {
+            if (chr == string.charAt(i))
+                sum++;
+        }
+        int[] arr = new int[sum];
+        for (int i = 0; i < string.length(); i++) {
+            if (chr == string.charAt(i)) {
+                arr[j] = i;
+                j++;
+            }
+        }
+        return arr;
     }
 }
