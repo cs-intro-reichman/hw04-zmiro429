e53ed6d58dde32b456f389b6292a6620d7a1c7b0
zmiro429 <adam.zmiro@post.runi.ac.il>
2024-01-16 15:48:05 +0200

    third time a charm

--- ArrayOps.java
+++ ArrayOps.java
@@ -26,27 +26,21 @@ public class ArrayOps {
 
     public static int secondMaxValue(int[] array) {
         // Write your code here:
-        int arrl = array.length;
-        int max = array[0], secondmax = array[0], finder = 0;
-        // this for loop to find the Max in the array
-        for (int j = 0; j < arrl; j++) {
-            if (max < array[j]) {
-                max = array[j];
-                finder = j;
+        int max = array[0];
+        int secondMax = array[0];
+        int maxCount = 1;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                secondMax = max;
+                max = array[i];
+                maxCount = 1;
+            } else if (array[i] == max) {
+                maxCount++;
+            } else if (array[i] > secondMax) {
+                secondMax = array[i];
             }
         }
-        for (int i = 0; i < arrl; i++) {
-            if (array[i] == max && i != finder)
-                return array[i];
-        }
-        for (int i = 0; i < arrl; i++) {
-            for (int j = 0; j < arrl; j++) {
-                int num = array[j];
-                if (num > secondmax && num <= max)
-                    secondmax = num;
-            }
-        }
-        return secondmax;
+        return maxCount > 1 ? max : secondMax;
     }
 
     public static boolean containsTheSameElements(int[] array1, int[] array2) {
@@ -68,24 +62,18 @@ public class ArrayOps {
 
     public static boolean isSorted(int[] array) {
         // Write your code here:
-        String arr1 = Arrays.toString(array);
-        boolean dec = true;
-        boolean inc = true;
-        for (int i = 1; i < arr1.length(); i++) {
-            if (dec && arr1.charAt(i - 1) < arr1.charAt(i))
-                dec = false;
-        }
-        for (int i = 0; i < arr1.length(); i++) {
-            if (inc && arr1.charAt(i - 1) > arr1.charAt(i))
-                inc = false;
-        }
-        if (inc == true)
+        if (array.length < 2) {
             return true;
-        else if (dec == true)
-            return true;
-        else if (!inc && !dec)
-            return false;
-        return false;
+        }
+        boolean increasing = array[0] <= array[1];
+        for (int i = 1; i < array.length - 1; i++) {
+            if (increasing && array[i] > array[i + 1]) {
+                return false;
+            } else if (!increasing && array[i] < array[i + 1]) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
